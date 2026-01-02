80d220a90ba6844edbb37bfedd899ad5e73b7631
Robert Adam <dev@robert-adam.de>
2026-01-02 20:08:36 +0100

    Adapt tests to include moved & duplicated objects

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,11 +3,19 @@
 #include <cstdio>
 #include <iostream>
 
+#define OSEVENTS_SETUP_TEST_INSTANCES(Class, name) \
+	Class name##_initial;                          \
+	Class name = std::move(name##_initial);        \
+	Class name##_additional;
+
 int main() {
-	osevents::SessionLock lockWatcher;
-	lockWatcher.register_callback([](osevents::SessionLockState state) {
-		std::cout << "Session lock state changed to " << static_cast< int >(state) << "\n";
-	});
+	OSEVENTS_SETUP_TEST_INSTANCES(osevents::SessionLock, lock_watcher);
+	lock_watcher.register_callback(
+		[](osevents::SessionLockState state) { std::cout << "Session lock state changed to " << state << "\n"; });
+
+	std::cout << "All event handlers set up. Waiting for effects...\nPress any key to exit\n\n";
 
 	std::getchar();
 }
+
+#undef OSEVENTS_SETUP_TEST_INSTANCES
